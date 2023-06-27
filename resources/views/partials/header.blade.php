9fc7b2d2233f4076941a4a7cb08a540dff75060a
damianolagana <damianolagana@gmail.com>
2023-06-27 18:13:50 +0200

    add navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,22 @@
-<header class="p-3">
-    <img class="logo" src="{{ Vite::asset('resources/img/logo.png') }}" alt="Laravel Logo">
-    <span>Some links: </span>
-    @foreach ($links as $link)
-        <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
-    @endforeach
+<header >
+    <nav id="navBar" class="navbar bg-body-tertiary">
+        <div class="container w-75">
+            <div>
+                <img class="logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Laravel Logo">
+            </div>
+            <div class="w-75 d-flex justify-content-evenly">
+                @foreach ($links as $link)
+                    <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
+                @endforeach 
+            </div>
+            <input type="text" name="" placeholder="Search">
+            
+           
+        
+        </div>
+    </nav>
+
+
+    
+
 </header>

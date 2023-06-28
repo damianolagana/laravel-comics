f765b11bcf0c455fcd63c8e2d43b73dbc4dfb101
damianolagana <damianolagana@gmail.com>
2023-06-28 11:17:55 +0200

    add main comic cards

--- config/comics.php
+++ config/comics.php
@@ -41,7 +41,7 @@ return [
     [
         "title" => "American Vampire 1976 #1",
         "description" => "America is broken. Trust between the government and the American public has crumbled. Paranoia reigns supreme. It’s 1976, and this is the concluding chapter of the Eisner Award-winning American Vampire! Skinner Sweet has exhausted all efforts to regain his lost immortality. With his powers and purpose gone, he is now determined to go out with a bang. At a seedy motorcycle rally in the desert where he’s closer than ever to his death wish, Pearl Jones and a shocking partner track him down for one last, desperate mission: The force known as the Gray Trader and its minions are tunneling through the bowels of the world to unleash hell on Earth—just in time for America’s bicentennial. With catastrophe looming, it’s up to Skinner and Pearl to reconcile and change the course of history—or die trying. The series that launched the careers of superstars Scott Snyder and Rafael Albuquerque returns for nine final issues and the closing chapter of the legacy of American Vampire.",
-        "thumb" => "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+        "thumb" => "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
         "price" => "$3.99",
         "series" => "American Vampire 1976",
         "sale_date" => "2020-10-06",
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,16 +6,16 @@
     <div class="row g-4">
         @foreach($comics as $comic)
         <div class="col-2 comiCard">
+            
             <div>
-                <div>
-                    <h4>{{$comic["title"]}}</h4>
-
-                </div>
-                <div>
-                    <img src="{{$comic["thumb"]}}" alt="">
-                    
-                </div>
+                <img src="{{$comic["thumb"]}}" alt="">
+                
             </div>
+            <div>
+                <h4>{{$comic["title"]}}</h4>
+
+            </div>
+            
         </div>
         @endforeach
     </div>
